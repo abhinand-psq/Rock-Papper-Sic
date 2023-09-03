e1dbb4e8048c561ae45648a8a3faa9f944bc1379
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2023-09-03 10:12:21 +0530

    Add all my files

---
+++ index.js
@@ -0,0 +1,9 @@
+let age=19
+
+if(18 <= age){
+console.log("you are eligible to drive");
+}else if(18 <= age){
+console.log(" you are not eligible");
+}else{
+    console.log("you are not eligible to drive");
+}
